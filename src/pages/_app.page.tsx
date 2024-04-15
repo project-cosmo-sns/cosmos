c9de711fec4d978d14fb608fd81e7dc53e697cb3
jiyong <parkjiy96@naver.com>
2024-04-15 16:10:16 +0900

    🐛 fix : App.tsx에서 styles경로 수정

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,4 +1,4 @@
-import '@/src/styles/globals.css';
+import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import QueryProvider from '../utils/QueryProvider';
 import { CookiesProvider } from 'react-cookie';
