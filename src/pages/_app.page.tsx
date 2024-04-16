4e5233bb3d093dd6722b86ed9a234d8931169a2c
yunsungLEE <chance07236@naver.com>
2024-04-16 16:29:22 +0900

    ✏️setting: public 폴더에 아이콘 추가

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,7 +1,7 @@
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import QueryProvider from '../utils/QueryProvider';
 import { CookiesProvider } from 'react-cookie';
+import '@/styles/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
