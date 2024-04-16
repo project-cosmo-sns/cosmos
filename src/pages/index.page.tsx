f21229414fae1cdc6dbee63a79181a1505b6e069
jiyong <parkjiy96@naver.com>
2024-04-16 21:01:13 +0900

    ✨ feat : popover크기조절

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,12 +1,3 @@
-import NavigaionBar from '@/components/Common/NavigationBar';
-import SideBar from '@/components/Common/SideBar';
-
 export default function Home() {
-  return (
-    <div>
-      <SideBar />
-      <br />
-      {/* <NavigaionBar /> */}
-    </div>
-  );
+  return <div>Home</div>;
 }
