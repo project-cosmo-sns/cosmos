eeb4facd3b92263018e401d695077c4afb1cf80c
jiyong <parkjiy96@naver.com>
2024-04-17 16:48:09 +0900

    ✏️ chore : /페이지 컴포넌트 테스트 삭제

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,12 +1,3 @@
-import NavigaionBar from '@/components/Common/Layout/NavigationBar';
-import SideBar from '@/components/Common/Layout/SideBar';
-
 export default function Home() {
-  return (
-    <div>
-      <SideBar />
-      <NavigaionBar />
-      home
-    </div>
-  );
+  return <div>home</div>;
 }
