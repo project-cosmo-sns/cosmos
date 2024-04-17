809676317b5cd5d360380b6eb3cd0bad4044b1a7
yunsungLEE <chance07236@naver.com>
2024-04-17 16:19:32 +0900

    📦chore: home 컴포넌트 정리

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,12 +1,3 @@
-import CommentButton from '@/components/Common/Buttons/CommentButton';
-import CommentInput from '@/components/Common/CommentInput';
-
 export default function Home() {
-  return (
-    <>
-      <div>Home</div>
-      <CommentInput placeholder="댓글을 입력해주세요" />
-      <CommentButton clickEvent={() => console.log('1')}>등록</CommentButton>
-    </>
-  );
+  return <div>Home</div>;
 }
