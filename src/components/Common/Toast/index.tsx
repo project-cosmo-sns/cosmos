c0b1900d2f10f1a4f871974cac3a35d3081854a3
jiyong <parkjiy96@naver.com>
2024-04-17 22:16:22 +0900

    ✏️ chore : 다시 커밋

--- src/components/Common/Toast/index.tsx
+++ src/components/Common/Toast/index.tsx
@@ -2,7 +2,7 @@ import styles from './Toast.module.scss';
 import classNames from 'classnames/bind';
 
 /**
- * Toast 컴포넌트 사용시 setTimeout을 이용하여 6000ms 후에 사라지도록 하는게 좋을 것 같아요 
+ * Toast 컴포넌트 사용시 setTimeout을 이용하여 6000ms 후에 사라지도록 하는게 좋을 것 같아요
  */
 
 const cn = classNames.bind(styles);
