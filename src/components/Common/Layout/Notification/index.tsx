b9324430167feffabfb1c7cc70b77e3fbd632ff7
심은주 <nyaknya@naver.com>
2024-04-18 17:30:12 +0900

    🐛 fix: 빌드 오류 잡기용 미완성 파일 임시 삭제

--- src/components/Common/Layout/Notification/index.tsx
+++ src/components/Common/Layout/Notification/index.tsx
@@ -1,14 +1,11 @@
-import PopOver from '@/components/Common/PopOverBox';
-
+// 린트가 뭐라고 해서 임시로 popover 삭제하고 push
 export default function Notification() {
   return (
-    <PopOver>
-      <ul>
-        <li>이 내부를</li>
-        <li>map으로 돌리겠지?</li>
-        <li>수요일 안에 만들기!</li>
-        <li>ref 설정 관련은 좀 봐야할것같음</li>
-      </ul>
-    </PopOver>
+    <ul>
+      <li>이 내부를</li>
+      <li>map으로 돌리겠지?</li>
+      <li>수요일 안에 만들기!</li>
+      <li>ref 설정 관련은 좀 봐야할것같음</li>
+    </ul>
   );
 }
