f2b93534873250b80df153517238e6222afdc485
yunsungLEE <chance07236@naver.com>
2024-04-18 13:45:10 +0900

    feature: termBadge ui 제작

--- src/components/Common/Badge/TermBadge/index.tsx
+++ src/components/Common/Badge/TermBadge/index.tsx
@@ -1,10 +1,15 @@
+import styles from './TermBadge.module.scss';
+import classNames from 'classnames/bind';
+
 interface TermBadgeTypes {
   term: number;
 }
 
+const cn = classNames.bind(styles);
+
 export default function TermBadge({ term }: TermBadgeTypes) {
   return (
-    <div>
+    <div className={cn('term-badge')}>
       <span>{term}기</span>
     </div>
   );
