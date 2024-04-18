fd54106bae96fd4722adb5e1b51bf942ec1bbb26
jiyong <parkjiy96@naver.com>
2024-04-18 13:39:32 +0900

    ♻️ refactor :  : navBar 인풋 교체

--- src/components/Common/Layout/NavigationBar/index.tsx
+++ src/components/Common/Layout/NavigationBar/index.tsx
@@ -2,6 +2,7 @@ import styles from './NavigationBar.module.scss';
 import Image from 'next/image';
 import classNames from 'classnames/bind';
 import Link from 'next/link';
+import SearchInput from '../../SearchInput';
 
 const cn = classNames.bind(styles);
 
@@ -12,7 +13,7 @@ export default function NavigaionBar() {
         <Image src="icon/logo.svg" alt="logo" width={100} height={15} />
       </Link>
       <div className={cn('input-wrapper')}>
-        <input type="text" placeholder="Search" className={styles.search} />
+        <SearchInput />
       </div>
     </div>
   );
