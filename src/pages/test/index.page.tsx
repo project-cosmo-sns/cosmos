20b39337e05c7541195cccbb0cf289e01d292f02
YU YEON LEE <doli0116@naver.com>
2024-04-19 14:52:25 +0900

    ✨ feat: Icon에 onClick prop 추가 및 onClick 옵션 있으면 cursor: pointer 스타일 추가

--- src/pages/test/index.page.tsx
+++ src/pages/test/index.page.tsx
@@ -1,85 +1,61 @@
-import ContentContainer from '@/components/Common/ContentContainer';
-import { useState } from 'react';
 import * as Icon from '@/components/Common/IconCollection';
 
 export default function TestPage() {
-  const [selectedOption, setSelectedOption] = useState<
-    'post' | 'feed' | 'scrap'
-  >('feed');
-
   return (
-    <>
+    <div
+      style={{
+        position: 'fixed',
+        left: '0',
+        top: '30px',
+        padding: '20px',
+        border: '1px solid #ccc',
+      }}
+    >
+      <p>
+        Sample <br /> <br />
+      </p>
       <div
         style={{
-          background: '#f3f3f3',
-          width: '100%',
-          height: '100vh',
-          padding: '20px',
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr',
+          gap: '10px',
         }}
       >
-        <ContentContainer
-          selectedOption={selectedOption}
-          setSelectedOption={setSelectedOption}
-          isMyProfile
-        >
-          컨텐츠 내용
-        </ContentContainer>
+        <Icon.AddIcon fill="#511264" />
+        <Icon.AddImageIcon />
+        <Icon.BackIcon />
+        <Icon.BellIcon />
+        <Icon.CameraIcon />
+        <Icon.CheckIcon />
+        <Icon.CloseIcon />
+        <Icon.CommentIcon />
+        <Icon.DeleteIcon />
+        <Icon.DoubleLeftIcon />
+        <Icon.DoubleRightIcon />
+        <Icon.DownIcon />
+        <Icon.EditIcon />
+        <Icon.EmojiIcon />
+        <Icon.EyeIcon />
+        <Icon.FeedIcon />
+        <Icon.FollowedIcon />
+        <Icon.FollowingIcon />
+        <Icon.GlassIcon />
+        <Icon.HomeIcon />
+        <Icon.KebabIcon />
+        <Icon.LeftIcon />
+        <Icon.LikeIcon />
+        <Icon.LikedIcon />
+        <Icon.LinkIcon />
+        <Icon.PostIcon />
+        <Icon.RightIcon />
+        <Icon.ScrapIcon />
+        <Icon.ScrapedIcon />
+        <Icon.SettingIcon />
+        <Icon.ShareIcon />
+        <Icon.UpIcon />
+        <Icon.UserIcon />
+        <Icon.XIcon />
       </div>
-      <div
-        style={{
-          position: 'fixed',
-          left: '800px',
-          top: '30px',
-          padding: '20px',
-          border: '1px solid #ccc',
-        }}
-      >
-        <p>
-          Sample <br /> <br />
-        </p>
-        <div
-          style={{
-            display: 'grid',
-            gridTemplateColumns: '1fr 1fr',
-            gap: '10px',
-          }}
-        >
-          <Icon.AddIcon fill="#511264" />
-          <Icon.AddImageIcon />
-          <Icon.BackIcon />
-          <Icon.BellIcon />
-          <Icon.CameraIcon />
-          <Icon.CheckIcon />
-          <Icon.CloseIcon />
-          <Icon.CommentIcon />
-          <Icon.DeleteIcon />
-          <Icon.DoubleLeftIcon />
-          <Icon.DoubleRightIcon />
-          <Icon.DownIcon />
-          <Icon.EditIcon />
-          <Icon.EmojiIcon />
-          <Icon.EyeIcon />
-          <Icon.FeedIcon />
-          <Icon.FollowedIcon />
-          <Icon.FollowingIcon />
-          <Icon.GlassIcon />
-          <Icon.HomeIcon />
-          <Icon.KebabIcon />
-          <Icon.LeftIcon />
-          <Icon.LikeIcon />
-          <Icon.LikedIcon />
-          <Icon.LinkIcon />
-          <Icon.PostIcon />
-          <Icon.RightIcon />
-          <Icon.ScrapIcon />
-          <Icon.ScrapedIcon />
-          <Icon.SettingIcon />
-          <Icon.ShareIcon />
-          <Icon.UpIcon />
-          <Icon.UserIcon />
-          <Icon.XIcon />
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
