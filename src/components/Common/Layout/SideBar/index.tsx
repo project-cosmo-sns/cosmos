8f277366c129c8d0357a56f7d1dd06bc06a7ebb1
jiyong <parkjiy96@naver.com>
2024-04-20 11:57:41 +0900

    � fix : 토스트 조건  수정

--- src/components/Common/Layout/SideBar/index.tsx
+++ src/components/Common/Layout/SideBar/index.tsx
@@ -3,14 +3,13 @@ import classNames from 'classnames/bind';
 import Link from 'next/link';
 import { useState, useRef } from 'react';
 import AddContentPopOver from '../../AddContentPopOver';
-import UseOutSideClick from '@/hooks/UseOutSideClick';
+import useOutSideClick from '@/hooks/useOutSideClick';
 import {
   HomeIcon,
   BellIcon,
   UserIcon,
   AddIcon,
   WarnIcon,
-  CheckIcon,
 } from '@/components/Common/IconCollection';
 import Toast from '@/components/Common/Toast';
 import { useRouter } from 'next/router';
@@ -20,7 +19,6 @@ export default function SideBar() {
   const [isPopOver, setIsPopOver] = useState(false);
   const [toast, setToast] = useState(false);
   const popOverRef = useRef<HTMLDivElement>(null);
-  const router = useRouter();
 
   const popOverClick = (e: React.MouseEvent<HTMLElement>) => {
     e.stopPropagation();
@@ -28,20 +26,14 @@ export default function SideBar() {
   };
 
   const profileClick = (e: React.MouseEvent<HTMLElement>) => {
-    e.preventDefault();
-    const getlocalStorage = localStorage?.getItem('id');
-    if (getlocalStorage) {
-      router.push('/profile');
-    } else {
-      setToast(true);
-      setTimeout(() => {
-        setToast(false);
-      }, 5000);
-    }
+    setToast(true);
+    setTimeout(() => {
+      setToast(false);
+    }, 5000);
   };
-  // 인증여부를 조건을 예비로 로컬스토리지로 했습니다. api나오면 바꿀예정
+  // 일단은 이렇게 사용하고 데이터 연결하고 나중에 수정
 
-  UseOutSideClick({ ref: popOverRef, callback: () => setIsPopOver(false) });
+  useOutSideClick({ ref: popOverRef, callback: () => setIsPopOver(false) });
 
   return (
     <div className={cn('sideBar-container')}>
