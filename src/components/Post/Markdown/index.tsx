0412e1ae24fb04c293df274285e457b045fa6699
YU YEON LEE <doli0116@naver.com>
2024-04-21 03:00:28 +0900

    ✨ feat: 마크다운 컴포넌트에 코드블럭 컴포넌트 적용

--- src/components/Post/Markdown/index.tsx
+++ src/components/Post/Markdown/index.tsx
@@ -1,8 +1,9 @@
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
+import CodeBlock from './CodeBlock';
 
-interface MarkdownContentProps {
+interface MarkdownContent {
   className: string;
   content: string;
 }
@@ -10,12 +11,20 @@ interface MarkdownContentProps {
 export default function MarkdownContent({
   className,
   content,
-}: MarkdownContentProps) {
+}: MarkdownContent) {
+  const renderImage = ({ ...props }) => (
+    <img style={{ maxWidth: '100%' }} {...props} alt="" />
+  );
+
   return (
     <Markdown
       className={className}
       remarkPlugins={[remarkGfm]}
       rehypePlugins={[rehypeRaw]}
+      components={{
+        code: CodeBlock,
+        img: renderImage,
+      }}
     >
       {content}
     </Markdown>
