77be2fe2bb47bd1f9e349b2cb2e91bff31c2afc3
YU YEON LEE <doli0116@naver.com>
2024-04-24 18:25:08 +0900

    ♻️ refactor: 임시 데이터 태그 아이디 제거

--- src/components/Post/PostEditor/index.tsx
+++ src/components/Post/PostEditor/index.tsx
@@ -1,4 +1,9 @@
-import { CategoryType, mockData } from '@/pages/post/[postId]/mockData';
+import {
+  CategoryType,
+  PostData,
+  Tag,
+  mockData,
+} from '@/pages/post/[postId]/mockData';
 import classNames from 'classnames/bind';
 import { useEffect, useState } from 'react';
 import CategoryList from '../CategoryList';
@@ -6,14 +11,17 @@ import styles from './PostEditor.module.scss';
 
 interface PostEditorProps {
   postId: string;
+  setData: (args: PostData) => void;
 }
 
 const cn = classNames.bind(styles);
 
-export default function PostEditor({ postId }: PostEditorProps) {
+export default function PostEditor({ postId, setData }: PostEditorProps) {
   // 임시로 기본값 postId와 같은 포스트 mockData에서 불러옴. 추후 요청해서 받아오도록 수정
   const [titleValue, setTitleValue] = useState('');
   const [contentValue, setContentValue] = useState('');
+  const [hashtags, setHashtags] = useState<Tag[]>([]);
+
   const [selectedCategory, setSelectedCategory] =
     useState<CategoryType>('공지사항');
 
@@ -23,6 +31,7 @@ export default function PostEditor({ postId }: PostEditorProps) {
       setSelectedCategory(postData.category);
       setTitleValue(postData.title);
       setContentValue(postData.content);
+      setHashtags(postData.tags);
     }
   }, [postId]);
 
--- src/pages/write/index.page.tsx
+++ src/pages/write/index.page.tsx
@@ -4,12 +4,25 @@ import PostEditor from '@/components/Post/PostEditor';
 import classNames from 'classnames/bind';
 import { useRouter } from 'next/router';
 import styles from './PostWritePage.module.scss';
+import { PostData, mockData } from '../post/[postId]/mockData';
+import { useState } from 'react';
 
 const cn = classNames.bind(styles);
 
 export default function PostWritePage() {
   const router = useRouter();
+  const [data, setData] = useState<PostData | undefined>();
   const { postId } = router.query;
+
+  // 포스트 등록 시 실행할 함수 임시로 생성. 추후 등록요청 보내고 받아온 postId에 해당하는 페이지로 이동
+  const handleSubmitPostData = (newData: PostData | undefined) => {
+    if (data) {
+      setData(newData);
+      mockData.push(data);
+    } else {
+      alert('데이터를 입력해주세요');
+    }
+  };
 
   return (
     <div className={cn('wrapper')}>
@@ -20,15 +33,11 @@ export default function PostWritePage() {
         className={cn('back')}
       />
       <h2 className={cn('title')}>{postId ? '포스트 수정' : '포스트 작성'}</h2>
-      <PostEditor postId={postId as string} />
+      <PostEditor postId={postId as string} setData={setData} />
       <div className={cn('button-container')}>
         <DefaultButton
           buttonType="button"
-          onClick={() =>
-            console.log(
-              '등록요청 보내고 받아온 postId에 해당하는 페이지로 이동',
-            )
-          }
+          onClick={() => handleSubmitPostData(data)}
           size="large"
           color="purple"
         >
