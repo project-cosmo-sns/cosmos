718abb135f117892bc063f841660a811ac5e7a1a
심은주 <nyaknya@naver.com>
2024-05-02 00:12:29 +0900

    💄 design: 오늘의 질문 디자인 적용

--- src/components/Common/TodayQuestion/index.tsx
+++ src/components/Common/TodayQuestion/index.tsx
@@ -1,33 +1,18 @@
-import { useState } from 'react';
-import { UpIcon, DownIcon } from '../IconCollection';
 import styles from './TodayQuestion.module.scss';
 import classNames from 'classnames/bind';
+import { RightIcon } from '../IconCollection';
 
 const cn = classNames.bind(styles);
 
 export default function TodayQuestion() {
-  const [isToggled, setIsToggled] = useState(true);
-
-  const toggleIcon = () => {
-    setIsToggled((prevState) => !prevState);
-  };
-
-  const iconToShow = isToggled ? (
-    <DownIcon fill="black" width="18" height="18" />
-  ) : (
-    <UpIcon fill="black" width="18" height="18" />
-  );
-
   return (
-    <div className={cn('question-container', { toggled: isToggled })}>
+    <div className={cn('question-container')}>
       <div className={cn('question-title')}>
         <strong>오늘의 질문</strong>
-        <div className={cn('toggle-icon')} onClick={toggleIcon}>
-          {iconToShow}
-        </div>
+        <RightIcon fill="#fff" className={cn('viewmore-icon')} />
       </div>
-      <h3 className={cn('question-text', { hidden: !isToggled })}>
-        질문내용? 여러분들 잘 작업하고 계신가요? 힘냅시당...
+      <h3 className={cn('question-text')}>
+        코드잇 스프린터 여러분들의 꽃길을 응원합니다 🌸
       </h3>
     </div>
   );
