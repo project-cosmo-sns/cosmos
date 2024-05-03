5827d36cda7b8ac503e010bca9839faf26f21b6e
yunsungLEE <chance07236@naver.com>
2024-05-03 19:05:08 +0900

    ✨feature: 피드 댓글 목록 get 요청

--- src/components/Feed/FeedDetails/api.ts
+++ src/components/Feed/FeedDetails/api.ts
@@ -5,3 +5,9 @@ export async function getFeedDetails(feedId: number) {
   const feedDetails = data.data;
   return feedDetails;
 }
+
+export async function getFeedCommentList(feedId: number) {
+  const data = await instance.get(`feed/${feedId}/comment/list`);
+  const feedCommentList = data.data.data;
+  return feedCommentList;
+}
--- src/components/Feed/FeedDetails/index.tsx
+++ src/components/Feed/FeedDetails/index.tsx
@@ -5,7 +5,7 @@ import FeedCard from '../FeedCard';
 import styles from './FeedDetails.module.scss';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { getFeedDetails } from './api';
+import { getFeedCommentList, getFeedDetails } from './api';
 
 /**
  * @return {JSX.Element} FeedDetails - 추후에 변경 예정입니다. 피드 리스트에서 특정 피드를 클릭한다면 클리한 피드의 아이디를 통해 데이터를 요청해 화면에 보여줍니다.
@@ -17,35 +17,48 @@ export default function FeedDetails() {
     writer: {},
     feed: {},
   });
+  const [commentList, setCommentList] = useState([]);
   const router = useRouter();
   const { query } = router;
   const feedId: number = Number(query.feedId);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchFeedDetails = async () => {
       const feedDetails = await getFeedDetails(feedId);
       setFeed(feedDetails);
     };
-    fetchData();
+    const fetchFeedComments = async () => {
+      const feedComments = await getFeedCommentList(feedId);
+      setCommentList(feedComments);
+    };
+
+    fetchFeedDetails();
+    fetchFeedComments();
   }, []);
 
   return (
     <div className={cn('container')}>
       <FeedCard feedData={feed} hasPadding={false} forDetails />
-      {/* <CommentInput
+      <CommentInput
         placeholder="댓글을 입력하세요"
         handleClick={() => console.log('등록')}
       />
-      <div>
-        {comments.map((comment, index) => (
+      {commentList.length ? (
+        commentList.map((comment, index) => (
           <div key={comment.id}>
             <CommentCard comment={comment} />
-            {index === comments.length - 1 || (
+            {index === commentList.length - 1 || (
               <div className={cn('divide-line')} />
             )}
           </div>
-        ))}
-      </div> */}
+        ))
+      ) : (
+        <div className={cn('empty-comment')}>
+          <span className={cn('message')}>
+            😭 {feed.writer.nickname} 님에게 남겨진 댓글이 아직 없어요. 😭
+          </span>
+        </div>
+      )}
     </div>
   );
 }
