cb98c54d28d246be17f44af2ce194786cf5a9a7a
심은주 <nyaknya@naver.com>
2024-05-05 14:02:07 +0900

    🔥 remove: 임시로 넣었던 코드 삭제

--- src/pages/search/index.page.tsx
+++ src/pages/search/index.page.tsx
@@ -4,6 +4,23 @@ import ContentContainer from '@/components/Common/ContentContainer';
 import SearchAuthorProfileList from '@/components/Search/SearchAuthorProfileList';
 import SearchList from '@/components/Search/SearchList';
 import { useState } from 'react';
+import instance from '@/api/axios';
+
+// 흠.... 쿼리값 어떻게 놓을지 고민해야됨 + 전역화
+
+export async function getSearchList() {
+  const feedData = await instance.get('/search/post/hash-tag?');
+  return feedData.data;
+}
+
+export const getServerSideProps = async () => {
+  const searchList = await getSearchList();
+  return {
+    props: {
+      feedList: searchList.data,
+    },
+  };
+};
 
 export default function SearchResultPage() {
   const [selectedOption, setSelectedOption] =
