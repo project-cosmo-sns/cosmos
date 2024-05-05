e6a5424724ca4c19ee9dcc7f7f69ea9956242ace
심은주 <nyaknya@naver.com>
2024-05-05 23:08:33 +0900

    ✨ feat: 로딩 스피너 추가 및 검색 해시태그 api 리액트 쿼리 사용

--- src/pages/search/index.page.tsx
+++ src/pages/search/index.page.tsx
@@ -1,61 +1,42 @@
 import { ContainerOptionType } from '@/@types/type';
 import { useSearchParams } from 'next/navigation';
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import ContentContainer from '@/components/Common/ContentContainer';
 import SearchAuthorProfileList from '@/components/Search/SearchAuthorProfileList';
 import SearchList from '@/components/Search/SearchList';
-import { useState, useEffect } from 'react';
-import { getSearchList } from '@/components/Search/api';
-import { SearchData } from '@/components/Search/type';
-
-// serverside 임시 주석
-// export const getServerSideProps = async ({ query }: any) => {
-//   const { keyword } = query;
-//   const searchList = await getSearchList(keyword);
-//   return {
-//     props: {
-//       searchList: searchList.data,
-//     },
-//   };
-// };
+import { SearchResult } from '@/components/Search/type';
+import NoSearchResult from '@/components/Search/NoSearchResult';
+import LoadingSpinner from '@/components/Common/LoadingSpinner';
+import fetchData from '@/api/fetchData';
 
 export default function SearchResultPage() {
   const [selectedOption, setSelectedOption] =
     useState<ContainerOptionType>('hashtag');
-  const [searchList, setSearchList] = useState<SearchData[]>([]);
-
   const searchParams = useSearchParams();
-  const search = searchParams.get('query');
-  const keyword = search || '';
+  const keyword: string = searchParams.get('query') || '';
 
-  const loadSearchData = async () => {
-    const fetchData = async () => {
-      try {
-        const result = await getSearchList(keyword);
-        setSearchList(result.data);
-      } catch (error) {
-        console.error('검색 결과 오류', error);
-      }
-    };
-
-    fetchData();
-  };
-
-  useEffect(() => {
-    if (keyword) {
-      loadSearchData();
-    }
-  }, [keyword]);
+  const { data: searchResult, isLoading } = useQuery<SearchResult>({
+    queryKey: ['searchList', keyword],
+    queryFn: () =>
+      fetchData({
+        param: `/search/post/hash-tag?order=DESC&page=1&take=4&keyword=${keyword}`,
+      }),
+  });
 
   let searchResultComponent;
-  if (searchList) {
+
+  if (isLoading) {
+    searchResultComponent = <LoadingSpinner />;
+  } else if (!searchResult || searchResult.data.length === 0) {
+    searchResultComponent = <NoSearchResult />;
+  } else {
     searchResultComponent =
       selectedOption === 'hashtag' ? (
-        <SearchList searchList={searchList} />
+        <SearchList searchList={searchResult.data} />
       ) : (
         <SearchAuthorProfileList />
       );
-  } else {
-    searchResultComponent = <p>검색 결과가 없습니다.</p>;
   }
 
   return (
