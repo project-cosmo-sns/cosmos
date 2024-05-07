e67481bd3dc16095ba056eeb759b9df0461f1fb3
Yoojungchoii <geniusyj@gmail.com>
2024-05-07 14:14:56 +0900

    ✨ feat : 내 피드 글 거르기 완성

--- src/api/ImageUploader.ts
+++ src/api/ImageUploader.ts
@@ -12,18 +12,8 @@ const uploadImage = async (fileData) => {
     requestData: formData, // 요청 본문에 전송할 데이터
   });
 
-  return response; // 이미지url이 오겟징...온다고햇다.
+  return response; //  어떤 데이터가 오라? 
 };
-
-export default function ImageUploader({ uploadURL }) {
-  const { mutate: uploadImage, isLoading, error } = useMutation(uploadProfileImage, {
-    onSuccess: () => {
-      alert('프로필 이미지가 성공적으로 업로드되었습니다!');
-    },
-    onError: (error) => {
-      console.log(`오류 발생: ${error.message}`);
-    },
-  });
 
   const handleFileChange = (event : ) => {
     const file = event.target.files[0];
