684363166f025c40ef387ef78d22d864d01d1023
심은주 <nyaknya@naver.com>
2024-05-08 05:21:25 +0900

    ✨ feat: 사용자 검색 디자인 수정, 팔로우/언팔로우 로직 입힘

--- src/components/Search/SearchAuthorProfile/index.tsx
+++ src/components/Search/SearchAuthorProfile/index.tsx
@@ -1,8 +1,11 @@
+import FollowButton from '@/components/Common/Buttons/FollowButton';
 import { Member } from '../type';
 import classNames from 'classnames/bind';
 import styles from './SearchAuthorProfile.module.scss';
 import Image from 'next/image';
 import GenerationBadge from '@/components/Common/GenerationBadge';
+import { useRouter } from 'next/router';
+import useFollowClick from '@/hooks/useFollowClick';
 
 interface SearchAuthorProfileProps {
   member: Member;
@@ -13,7 +16,10 @@ const cn = classNames.bind(styles);
 export default function SearchAuthorProfile({
   member,
 }: SearchAuthorProfileProps) {
+  const router = useRouter();
+
   const {
+    id,
     nickname,
     generation,
     profileImageUrl,
@@ -24,49 +30,56 @@ export default function SearchAuthorProfile({
     isMine,
   } = member;
 
-  console.log(isFollowing);
-
-  // isFollowing은 팔로우 버튼 변하는거에 따라서... 수정해서 넣어야 할것같습니다! 그래서 일단 false로 통일합니다.
+  const { toggleFollow } = useFollowClick(id);
 
   return (
     <div className={cn('search-profile')}>
-      <Image
-        className={cn('profile-image')}
-        src={profileImageUrl || '/images/profile.svg'}
-        alt="profile_image"
-        width={86}
-        height={86}
-        onClick={() => console.log('프로필모달 열기')}
-      />
-      <div className={cn('profile-infomation')}>
-        <div className={cn('profile-name')}>
-          <h3>{nickname}</h3>
-          <GenerationBadge
-            isAuthorized={!!generation}
-            generationInfo={generation}
-          />
-        </div>
-        <div className={cn('profile-follow')}>
-          <div>
-            <span>팔로워</span>
-            <strong>{followerCount}</strong>
+      <div
+        className={cn('profile-content')}
+        onClick={() => router.push(`/profile?memberId=${id}`)}
+      >
+        <Image
+          className={cn('profile-image')}
+          src={profileImageUrl || '/images/profile.svg'}
+          alt="profile_image"
+          width={86}
+          height={86}
+        />
+        <div className={cn('profile-infomation')}>
+          <div className={cn('profile-name')}>
+            <h3>{nickname}</h3>
+            <GenerationBadge
+              isAuthorized={!!generation}
+              generationInfo={generation}
+            />
           </div>
-          <div>
-            <span>팔로잉</span>
-            <strong>{followingCount}</strong>
+          <div className={cn('profile-follow')}>
+            <div>
+              <span>팔로워</span>
+              <strong>{followerCount}</strong>
+            </div>
+            <div>
+              <span>팔로잉</span>
+              <strong>{followingCount}</strong>
+            </div>
           </div>
-        </div>
-        <div className={cn('profile-description')}>
-          <p>{introduce || '소개가 없습니다.'}</p>
+          <div className={cn('profile-description')}>
+            <p>{introduce || '소개가 없습니다.'}</p>
+          </div>
         </div>
       </div>
 
-      {/* {!isMine && (
+      <div className={cn('profile-description-mobile')}>
+        <p>{introduce || '소개가 없습니다.'}</p>
+      </div>
+
+      {!isMine && (
         <FollowButton
-          onClick={() => console.log('팔로우클릭')}
-          isFollow={false}
+          onClick={toggleFollow}
+          isFollowButton
+          isActive={!isFollowing}
         />
-      )} */}
+      )}
     </div>
   );
 }
