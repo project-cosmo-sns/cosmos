a7aa5ea5cd372ff6c7e8f80874d749f596a036e3
YU YEON LEE <doli0116@naver.com>
2024-05-09 15:21:33 +0900

    ✨ feat: 포스트 데이터 로딩중일때 로딩중 띄우기
    - 추후 스켈레톤 ui로 변경 예정

--- src/components/Post/PostContent/index.tsx
+++ src/components/Post/PostContent/index.tsx
@@ -4,40 +4,44 @@ import DeleteModal from '@/components/Common/DeleteModal';
 import EmojiBundle from '@/components/Common/EmojiBundle';
 import WriterProfile from '@/components/Common/WriterProfile';
 import useSendEmojiRequest from '@/hooks/useSendEmojiRequest';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import classNames from 'classnames/bind';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import HashTag from '../HashTag';
 import MarkdownContent from '../Markdown';
 import PostComment from '../PostComment';
 import { HashTagType, PostDetailType } from '../types';
 import styles from './PostContent.module.scss';
 
-interface PostContentProps {
-  postData: PostDetailType;
-}
-
 const cn = classNames.bind(styles);
 
-export default function PostContent({ postData }: PostContentProps) {
+export default function PostContent() {
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-
   const router = useRouter();
   const { postId } = router.query;
 
-  const { post, writer } = postData.postDetail;
   const {
-    category,
-    title,
-    createdAt,
-    content,
-    hashTags,
-    emojis,
-    viewCount,
-    commentCount,
-    isMine,
-  } = post;
+    data: postData,
+    isSuccess,
+    isPending,
+    isError,
+  } = useQuery<PostDetailType>({
+    queryKey: ['postData', postId],
+    queryFn: () =>
+      fetchData({
+        param: `/post/${postId}/detail`,
+      }),
+    enabled: !!postId,
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: () =>
+      fetchData({
+        param: `/post/${postId}/view-count/increase`,
+        method: 'post',
+      }),
+  });
 
   const { mutate: deleteMutate } = useMutation({
     mutationFn: () =>
@@ -51,42 +55,63 @@ export default function PostContent({ postData }: PostContentProps) {
   const { handleEmojiClick, isAddPending, isDeletePending } =
     useSendEmojiRequest(Number(postId), true);
 
-  return (
-    <div className={cn('wrapper')}>
-      <span className={cn('category')}>{category}</span>
-      <span className={cn('title')}>{title}</span>
-      <div className={cn('header')}>
-        <WriterProfile
-          writer={writer}
-          createdAt={createdAt.slice(0, 10).replace(/-/g, '.')}
+  useEffect(() => {
+    if (postId && isSuccess) mutate();
+  }, [postId, isSuccess]);
+
+  if (isPending) <>...Loading</>;
+  if (isError) <>로그인모달</>;
+  if (isSuccess) {
+    const { post, writer } = postData.postDetail;
+    const {
+      category,
+      title,
+      createdAt,
+      content,
+      hashTags,
+      emojis,
+      viewCount,
+      commentCount,
+      isMine,
+    } = post;
+
+    return (
+      <div className={cn('wrapper')}>
+        <span className={cn('category')}>{category}</span>
+        <span className={cn('title')}>{title}</span>
+        <div className={cn('header')}>
+          <WriterProfile
+            writer={writer}
+            createdAt={createdAt.slice(0, 10).replace(/-/g, '.')}
+          />
+          <ActionButtons
+            isButtonShow={isMine}
+            handleClickEdit={() => router.push(`/write?postId=${postId}`)}
+            handleClickDelete={() => setIsDeleteModalOpen(true)}
+          />
+          <DeleteModal
+            isDeleteModalOpen={isDeleteModalOpen}
+            setIsDeleteModalOpen={setIsDeleteModalOpen}
+            handleDelete={deleteMutate}
+          />
+        </div>
+        <div className={cn('divide-line')} />
+        <MarkdownContent className={cn('markdown-content')} content={content} />
+        <div className={cn('hashtag-container')}>
+          {hashTags.map((tag: HashTagType) => (
+            <HashTag key={`${tag.tagName}`} tag={tag} />
+          ))}
+        </div>
+        <EmojiBundle
+          isPost
+          commentCount={commentCount}
+          viewCount={viewCount}
+          emojiList={emojis}
+          handleEmojiClick={handleEmojiClick}
+          isPending={isAddPending || isDeletePending}
         />
-        <ActionButtons
-          isButtonShow={isMine}
-          handleClickEdit={() => router.push(`/write?postId=${postId}`)}
-          handleClickDelete={() => setIsDeleteModalOpen(true)}
-        />
-        <DeleteModal
-          isDeleteModalOpen={isDeleteModalOpen}
-          setIsDeleteModalOpen={setIsDeleteModalOpen}
-          handleDelete={deleteMutate}
-        />
+        <PostComment postId={Number(postId)} />
       </div>
-      <div className={cn('divide-line')} />
-      <MarkdownContent className={cn('markdown-content')} content={content} />
-      <div className={cn('hashtag-container')}>
-        {hashTags.map((tag: HashTagType) => (
-          <HashTag key={`${tag.tagName}`} tag={tag} />
-        ))}
-      </div>
-      <EmojiBundle
-        isPost
-        commentCount={commentCount}
-        viewCount={viewCount}
-        emojiList={emojis}
-        handleEmojiClick={handleEmojiClick}
-        isPending={isAddPending || isDeletePending}
-      />
-      <PostComment postId={Number(postId)} />
-    </div>
-  );
+    );
+  }
 }
