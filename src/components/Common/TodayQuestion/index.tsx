4356a5ce669f2831160946af297fd38bae6ebf8c
심은주 <nyaknya@naver.com>
2024-05-13 16:02:47 +0900

    🔥 remove: 오늘의 질문 미인증이어도 보여서 임시로 추가했던 컴포넌트 삭제

--- src/components/Common/TodayQuestion/index.tsx
+++ src/components/Common/TodayQuestion/index.tsx
@@ -5,14 +5,13 @@ import { useQuery } from '@tanstack/react-query';
 import { TodayQuestionResult } from './type';
 import fetchData from '@/api/fetchData';
 import { useRouter } from 'next/router';
-import Unauthorized from './Unauthorized';
 
 const cn = classNames.bind(styles);
 
 export default function TodayQuestion() {
   const router = useRouter();
 
-  const { data, isError } = useQuery<TodayQuestionResult>({
+  const { data } = useQuery<TodayQuestionResult>({
     queryKey: ['todayQuestion'],
     queryFn: () =>
       fetchData({
@@ -25,17 +24,11 @@ export default function TodayQuestion() {
       className={cn('question-container')}
       onClick={() => router.push(`/post/${data?.postId}`)}
     >
-      {isError ? (
-        <Unauthorized />
-      ) : (
-        <>
-          <div className={cn('question-title')}>
-            <strong>오늘의 질문</strong>
-            <RightIcon fill="#fff" className={cn('viewmore-icon')} />
-          </div>
-          <h3 className={cn('question-text')}>{data?.title}</h3>
-        </>
-      )}
+      <div className={cn('question-title')}>
+        <strong>오늘의 질문</strong>
+        <RightIcon fill="#fff" className={cn('viewmore-icon')} />
+      </div>
+      <h3 className={cn('question-text')}>{data?.title}</h3>
     </div>
   );
 }
