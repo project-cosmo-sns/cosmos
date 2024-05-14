c5d4c6c8e48fbfe8a78897154c7366173451e96c
jiyong <parkjiy96@naver.com>
2024-05-14 15:37:05 +0900

    chore : window.focus추가

--- src/api/Oauth.ts
+++ src/api/Oauth.ts
@@ -4,6 +4,7 @@ export async function githubLogin(code: string) {
   try {
     const res = await axios.post(`/auth/github/redirect?code=${code}`);
     window.opener.location.reload();
+    window.focus();
     window.close();
   } catch (error) {
     console.error('요청 실패:', error);
