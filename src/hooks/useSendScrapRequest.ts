0845dcc3849798913f9492ddcf8f13d7f22ad1db
YU YEON LEE <doli0116@naver.com>
2024-05-26 20:19:04 +0900

    ✨ feat: 스크랩 성공 시 토스트 띄우기

--- src/hooks/useSendScrapRequest.ts
+++ src/hooks/useSendScrapRequest.ts
@@ -1,13 +1,19 @@
 import fetchData from '@/api/fetchData';
 import { useMutation } from '@tanstack/react-query';
+import { useToast } from './useToast';
 
 export default function useSendScrapRequest({ postId }: { postId: number }) {
+  const { showToastHandler } = useToast();
+
   const { mutate: addScrapMutate, isPending: isAddPending } = useMutation({
     mutationFn: () =>
       fetchData({
         param: `/post/${postId}/scrap`,
         method: 'post',
       }),
+    onSuccess: () => {
+      showToastHandler('스크랩에 저장되었습니다.', 'check');
+    },
   });
 
   const { mutate: deleteScrapMutate, isPending: isDeletePending } = useMutation(
