14f858b030405c2bbc9ef0494144252dc56221d4
YU YEON LEE <doli0116@naver.com>
2024-06-30 00:01:33 +0900

    ♻️ refactor: svg 태그 속성 camelCase로 변경

--- src/components/Post/ScrapButton/index.tsx
+++ src/components/Post/ScrapButton/index.tsx
@@ -35,9 +35,9 @@ export default function ScrapButton({
         <path
           d="M21 2.66667V22.1315L12.8321 16.6862C12.3282 16.3503 11.6718 16.3503 11.1679 16.6862L3 22.1315V2.66667C3 2.24777 3.17766 1.83083 3.51916 1.5121C3.86298 1.1912 4.34319 1 4.85714 1H19.1429C19.6568 1 20.137 1.1912 20.4808 1.5121C20.8223 1.83083 21 2.24777 21 2.66667Z"
           stroke="#A0A5BB"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
       {/* <span className={cn('scrap-count')}>{scrapCount}</span> */}
