fd7a1d9a77874e8f87b3b7ef8f4381ad056c7a2b
yunsungLEE <chance07236@naver.com>
2024-07-10 19:28:22 +0900

    chore: pwa 줌 기능 disable 메타 태그 추가

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -248,6 +248,10 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#212531" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
       </Head>
       <body>
         <Main />
