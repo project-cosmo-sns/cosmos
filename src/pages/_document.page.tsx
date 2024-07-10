4eb836383e66e8972cf5d57a23dff8effcbfa674
yunsungLEE <chance07236@naver.com>
2024-07-10 17:10:27 +0900

    fix: link rel 수정

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -10,122 +10,122 @@ export default function Document() {
         <link
           href="splashscreens/iphone5_splash.png"
           media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
         />
         <link
           href="splashscreens/iphone6_splash.png"
           media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
         />
         <link
           href="splashscreens/iphoneplus_splash.png"
           media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
         />
         <link
           href="splashscreens/iphonex_splash.png"
           media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
         />
         <link
           href="splashscreens/iphonexr_splash.png"
           media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
         />
         <link
           href="splashscreens/iphonexsmax_splash.png"
           media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
         />
         <link
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
           media="(device-width: 360px) and (device-height: 780px) and (-webkit-device-pixel-ratio: 3)"
           href="splashscreens/iphonex_splash.png"
         />
         <link
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
           media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)"
           href="splashscreens/iphonex_splash.png"
         />
         <link
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
           media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)"
           href="splashscreens/iphonex_splash.png"
         />
         <link
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
           media="(device-width: 360px) and (device-height: 780px) and (-webkit-device-pixel-ratio: 3)"
           href="splashscreens/iphonex_splash.png"
         />
         <link
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
           media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)"
           href="splashscreens/iphonex_splash.png"
         />
         <link
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
           media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)"
           href="splashscreens/iphonex_splash.png"
         />
         <link
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
           media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)"
           href="splashscreens/iphonex_splash.png"
         />
         <link
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
           media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)"
           href="splashscreens/iphonex_splash.png"
         />
         <link
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
           media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)"
           href="splashscreens/iphonex_splash.png"
         />
         <link
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
           media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3)"
           href="splashscreens/iphonex_splash.png"
         />
         <link
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
           media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)"
           href="splashscreens/iphonex_splash.png"
         />
         <link
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
           media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)"
           href="splashscreens/iphonex_splash.png"
         />
         <link
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
           media="(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3)"
           href="splashscreens/iphonex_splash.png"
         />
         <link
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
           media="(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3)"
           href="splashscreens/iphonex_splash.png"
         />
         <link
           href="splashscreens/iphonex_splash.png"
           media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
         />
         <link
           href="splashscreens/iphonex_splash.png"
           media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
         />
         <link
           href="splashscreens/iphonex_splash.png"
           media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
         />
         <link
           href="splashscreens/iphonex_splash.png"
           media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
-          rel="apple-touch-startup-image"
+          rel="apple-touch-icon"
         />
       </Head>
       <body>
